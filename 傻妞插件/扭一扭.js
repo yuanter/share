c78ae84f5a3b228a58733d8432d086b5a890c575
yuanter <1369359503@qq.com>
2022-10-10 16:54:19 +0800

    更新部分插件

---
+++ 傻妞插件/扭一扭.js
@@ -0,0 +1,12 @@
+// 扭一扭
+// [rule: 视频]
+function main() {
+    var url = "http://muvip.cn/admin/WM/video.php?t=" + Date.now();
+    data = request({
+		url: url,
+		dataType: "location",
+	});
+    sendVideo(data)
+}
+
+main()
