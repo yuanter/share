a45549c3fb7a24789df2b5a1a0a3893b8bc1ac27
yuanter <1369359503@qq.com>
2023-03-15 02:57:28 +0800

    新增

--- 傻妞插件/jd_cookie和flycloud上车教程by旧版傻妞and凹凸曼.js
+++ 傻妞插件/jd_cookie和flycloud上车教程by旧版傻妞and凹凸曼.js
@@ -9,38 +9,16 @@
 // [disable: false] 是否禁用
 // [admin: false] 是否只允许管理员使用
 
-//当前版本适合配合jd_cookie v16.2以上版本使用
-// 使用口令和修改脚本内容，二选一的方式（选择口令，就不用修改脚本，若改了脚本就不用回复口令）
 
+// 设置jd_cookie或者flycloud地址，傻妞口令用法，对傻妞回复命令
+// set otto jd_cookieAddr http://xxx.xxx.xxx.xxx:1170      地址后面不带斜杆
 
-// 设置使用对应的容器，在网页处可看顺序，默认使用第一个容器，从0开始顺序往下数。傻妞口令用法，对傻妞回复命令
-// set otto jd_cookieKey 此处填容器的对应的key
-// 设置短信地址，傻妞口令用法，对傻妞回复命令
-// set otto jd_cookieAddr 这段文字修改为你的短信登录地址http://xxx.xxx.xxx.xxx:1170后面不带斜杆
-// 设置自定义机器人回复，如“傻妞为您服务”
-// set otto jd_cookieTip 这段文字修改为你的短信登录的提示文字，如傻妞为您服务
 
 var addr = get("jd_cookieAddr")
 if (!addr || addr == "" || addr == null) {
     //自行替换,如果未设置信息，请手动在这修改成自己jd_cookie的ip地址和端口，最后面不要带“/” ，不然会出错！
     addr = "XXX"
 }
-
-var key = get("jd_cookieKey")
-if (!key || key == "" || key == null) {
-    //自行替换,默认使用第一个容器，从0开始顺序往下数
-    key = "0"
-}
-var tip = get("jd_cookieTip")
-if (!tip || tip == "" || tip == null) {
-    //自行替换,如果未设置信息，请手动在这修改替换文字
-    tip = "通用CK上车咯~"
-}
-
-
-
-const user = GetUserID()
-const userName = GetUsername()
 console.log(addr)
 
 function main() {
